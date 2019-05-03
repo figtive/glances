8801a11b40c420c5f5d67a4c2e4a3c911dbcfde5
Kenia Visakha Zerlinda <zerlinda1122@gmail.com>
2019-05-03 11:53:50 +0700

    fix conflict

--- src/main/java/myHomepage/notes/Note.java
+++ src/main/java/myHomepage/notes/Note.java
@@ -7,25 +7,26 @@ public class Note{
 
     public Note(){ }
 
-    public Note (int id, String note){
+    public Note (int id, String note) {
         this.note = note;
         this.id = id;
     }
-    public void setdescription(String description){
+
+    public void setdescription(String description) {
         this.note = description;
     }
 
-    public String getdescription(){
+    public String getdescription() {
         return note;
     }
 
-    public String editnotes(String desc){
+    public String editnotes(String desc) {
         setdescription(desc);
         //this.note = note + " " + desc;
         return note;
     }
 
-    public String deletenotes(){
+    public String deletenotes() {
         this.note = nullnote;
         return note;
     }
